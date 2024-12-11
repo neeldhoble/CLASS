35355521da6d1818830df091bdc78ff45a9cd80a
neeldhoble <neeldhoble2004@gmail.com>
2024-12-11 17:29:25 +0530

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,62 @@
 import React from "react";
-import abc from "../assets/ABC.jpg"
+import abc from "../assets/edu.avif"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowDown, faCircleChevronLeft, faCircleChevronRight } from '@fortawesome/free-solid-svg-icons';
+import video from "../assets/video.avif"
+
 
 const Header = () => {
   return (
-    <div className="w-10/12 max-w-[1080px] flex flex-col lg:flex-row justify-between mx-auto items-center mt-5">
-      <div>hvgv</div>
-      <div className="w-[50%] flex flex-row">
-        <div className="w-[50%]">lkhfhebf</div>
-        <div className="w-[50%]"><img src={abc} alt="abc"className="rounded-lg"/></div>
+    <div className="w-10/12 max-w-[1080px] flex flex-col lg:flex-row justify-between mx-auto  mt-5">
+      <div className="flex flex-row h-[35px] justify-between gap-40 relative">
+              <div className="flex flex-row gap-2 h-[35px]">
+                  <div className="bg-white text-black p-1 rounded-lg">
+                    SmartClass 
+                  </div>
+                  <div className="bg-white text-black p-1 rounded-lg">
+                      menu <FontAwesomeIcon icon={faArrowDown} className="text-black"/>
+                  </div>
+                  <div className="border rounded-lg w-[100px]">
+
+                  </div>
+              </div>
+
+              <div className="flex flex-row gap-2 text-white items-center">
+                  <FontAwesomeIcon icon={faCircleChevronLeft}/>
+                  <FontAwesomeIcon icon={faCircleChevronRight}/>
+              </div>
+
+              <div className="absolute w-[150px] right-0 top-60">
+                <p className="absolute bg-white text-black p-1 rounded-lg ml-1 mt-1">video lesson</p>
+                <p className="absolute bg-gray-400 right-6 bottom-2 w-[50px] flex items-center justify-center rounded-lg">...</p>
+                <img src={video} alt="" />
+                
+              </div>
+      </div>
+
+
+
+      <div className="w-[50%] flex flex-row gap-3 relative">
+        {/* <div className="w-[50%] border">lkhfhebf</div> */}
+        <div className="rounded-[30px]"><img src={abc} alt="abc"className="rounded-[30px]"/></div>
+        <div className="absolute top-[300px] left-5 leading-7 font-bold text-[28px]">
+          <p>Explore all benefits</p>
+          <p>of SmartClass</p>
+        </div>
+        <div className="border p-2 font-bold text-[20px] absolute rounded-[20px] rotate-12 top-40 right-0">
+          Real-time class
+        </div>
+        <div className="border p-2 text-[20px] absolute rounded-[20px] top-72 right-14 rotate-1 font-bold">
+          interactive
+        </div>
+
+        <div className="border p-2 text-[20px] absolute rounded-[20px] top-60 right-32 -rotate-12 font-bold">
+            Multimedia
+        </div>
+
+        <div  className="border p-1 absolute rounded-full bottom-5 right-2 font-bold">
+        <FontAwesomeIcon icon={faCircleChevronRight}/>
+        </div>
       </div>
     </div>
   );
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,67 @@
 import React from "react";
+import linkedin from "../assets/linkedin.png"
+import insta from "../assets/insta.png"
+import facebook from "../assets/facebook.png"
+// import arrow from "../assets/arrow.png"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowUpRightDots, faFaceSmile, faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+
+
+
 
 const HeroSection = () => {
   return (
-      <div className="w-10/12 max-w-[1080px] flex flex-col lg:flex-row justify-between mx-auto items-center mt-10">
-        <div className="w-[50%] bottom-0">
-          <p className="text-[35px]">Transforming education with SmartClas"</p>
-          <p>Welcome to SmartClass:</p>
+      <div className="w-10/12 max-w-[1080px] flex flex-col lg:flex-row justify-between mx-auto items-center mt-5">
+        <div className="w-[50%] bottom-0 leading-none">
+          <p className="text-[50px]">Transforming</p>
+          <p className="text-[50px]">education with</p>
+          <p className="text-[50px]">SmartClass <FontAwesomeIcon icon={faQuoteLeft} className="w-[20px]"/></p>
+          <p className="mt-5">Welcome to SmartClass:</p>
           <p>innovation classroom online platform.</p>
         </div>
 
         <div className="w-[50%] flex flex-row gap-3">
-          <div className="bg-blue-500 h-[100px] w-[50%]">
-            <p>ljgyh</p>
+          <div className="bg-[#9D72B3] h-[290px] w-[40%] p-4 rounded-[10px]">
+            <div className="bg-white shadow-lg h-[100%]  shadow-lg shadow-black text-black flex flex-col relative">
+              <p className="text-[20px] mx-auto mt-5 font-bold">Connect With Us</p>
+              <p className="text-[14px] ml-3 mr-1">We’d love to hear from you! Whether you have questions or inquiries, feel free to reach out to us.</p>
+              <p className="text-[10px] ml-3 mt-3">📧 Email Us: xyz@gmail.com</p>
+              <p className="text-[10px] ml-3">📞 Call Us: +91 274656856</p>
+              <div className="bg-black w-[90%] rounded-[30px] mt-8 mx-auto p-1 flex gap-2">
+                <div className="flex gap-3 ml-4 mx-auto">
+                    <img src={linkedin} alt="" className="w-[30px]"/>
+                    <img src={facebook} alt="" className="w-[30px] ml-1"/>
+                    <img src={insta} alt="" className="w-[40px]"/>
+                </div>
+              </div>
+              
+
+            </div>
+
+            
+            
           </div>
-          <div className="bg-blue-500 h-[100px] w-[50%]">
-            <p>hjgjhg</p>
+          <div className="bg-[#9D72B3] h-[290px] w-[60%] rounded-[10px] p-4">
+            <div className="flex justify-between">
+                <div className="border p-2 w-[60%] rounded-[10px]">
+                  Opportunities
+                </div>
+
+                <div className="flex gap-1">
+                      <div className="rounded-full border p-2 flex items-center justify-center">
+                      <FontAwesomeIcon icon={faFaceSmile} className="border p-1 rounded-full"/>
+                      </div>
+                      
+                      <div className="rounded-full border p-3 flex items-center justify-center">
+                      <FontAwesomeIcon icon={faArrowUpRightDots} className="text-black"/>
+                      </div>
+                </div>
+    
+            </div>
+
+            <h1 className="font-bold mt-8 text-[20px]">Embrace the Future</h1>
+            <h1 className="font-bold text-[20px]">of Education</h1>
+            <p className="text-[14px]">Our online Platform allowing to connect, collaborate, & thrive in a dynamic environment.</p>
           </div>
         </div>
       </div>
